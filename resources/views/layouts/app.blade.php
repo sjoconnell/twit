c1b6e70ee81e209cd0b639fcd5b049e237138ed9
Samuel O'Connell <soconnell@Samuels-MacBook-Air.local>
2016-02-16 17:31:09 -0600

    changed core.js location

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
         <link href='https://fonts.googleapis.com/css?family=Erica+One' rel='stylesheet' type='text/css'>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <script type="text/javascript" src="{{asset('javascript/core.js')}}"></script>
+        <script type="text/javascript" src="{{asset('css/core.js')}}"></script>
 
     </head>
 
