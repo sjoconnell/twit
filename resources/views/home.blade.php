fe29f38f1174f7353e6408b11eb985cb448e14ee
Samuel O'Connell <soconnell@Samuels-MacBook-Air.local>
2016-02-16 11:17:42 -0600

    added styling to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,27 +10,43 @@
             
 <div class="welcome-body">
     <div class="wrapper">
-        <div class="profile-box">
-            <form action="{{ url('twitter/logout') }}" method="GET" class="logout-button">
-                {!! csrf_field() !!}
+        <div class="left-box">
+            <div class="profile-box">
+                <img src="{{$twit_image}}" class="twitter-image">
 
-                <button type="submit" class="btn btn-default">Logout</button>
-            </form>
+                <h1>{{$twit_name}}</h1>
+                <br>
+
+                <p>@ {{$twit_screen_name}}</p>
+
+                <form action="{{ url('twitter/logout') }}" method="GET" class="logout-button">
+                    {!! csrf_field() !!}
+
+                    <button type="submit" class="btn btn-default">Logout</button>
+                </form>
+
+            </div>
+
+            <div class="tweet-box">
+                <form action="{{ url('/tweet') }}" method="POST" class="tweet-button">
+                    {!! csrf_field() !!}
+                    <textarea type="text" name="tweet_text" maxlength="140" required="required" ></textarea>
+
+                    <button type="submit" class="btn btn-default">Tweet</button>
+                </form>
+            </div>
         </div>
 
-        <form action="{{ url('/tweet') }}" method="POST" class="tweet-button">
-            {!! csrf_field() !!}
-            <textarea type="text" name="tweet_text" maxlength="140" required="required" ></textarea>
-
-            <button type="submit" class="btn btn-default">Tweet</button>
-        </form>
-
-        <br>
-        <ul>
-            @foreach ($my_tweets as $tweet)
-            <li>{{ $tweet['text'] }}</li>
-             @endforeach
-         </ul>
+        <div class="right-box">
+            <div class="your-tweet-box">
+                <h1>Your Tweets</h1>
+                <ul>
+                    @foreach ($my_tweets as $tweet)
+                    <li>{{ $tweet['text'] }}</li>
+                     @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 </div>         
 
