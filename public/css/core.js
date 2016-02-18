20d07a6d59d5bd7b063563df3fa55caa10a62a4c
Samuel O'Connell <soconnell@Samuels-MacBook-Air.local>
2016-02-17 21:51:03 -0600

    fixed jquery logic for my tweets and timeline

--- public/css/core.js
+++ public/css/core.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 
     $(".tweets-left").click(function(){
-        $(this).toggleClass("grey-color");
-        $(".timeline-right").toggleClass("grey-color");
-        $(".your-tweet-box").toggleClass("invisible");
-        $(".your-timeline-box").toggleClass("invisible");
+        if ($(this).hasClass("grey-color")){
+            $(this).toggleClass("grey-color");
+            $(".timeline-right").toggleClass("grey-color");
+            $(".your-tweet-box").toggleClass("invisible");
+            $(".your-timeline-box").toggleClass("invisible");
+        }
     });
 
     $(".timeline-right").click(function(){
-        $(this).toggleClass("grey-color");
-        $(".tweets-left").toggleClass("grey-color");
-        $(".your-timeline-box").toggleClass("invisible");
-        $(".your-tweet-box").toggleClass("invisible");
+        if ($(this).hasClass("grey-color")){
+            $(this).toggleClass("grey-color");
+            $(".tweets-left").toggleClass("grey-color");
+            $(".your-timeline-box").toggleClass("invisible");
+            $(".your-tweet-box").toggleClass("invisible");
+        }
     });
     
 });
